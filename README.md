56a2377823de927d72cbcba087a545994fcf3c78
Ujjwal Agrahari <agrahari.ujjwal03@gmail.com>
2025-07-23 21:58:47 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+the react native based app can be run by firstly installing the dependencies using the command 
+
+npm install react-native-paper react-native-image-picker expo-image-picker expo-router expo-device expo-font expo-sharing expo-file-system @react-native-picker/picker @expo/vector-icons react-native-appwrite
+
+then running the command "npm run start" this will open the qr code for the expo app that can be run both on the android simulator as well as in the localhost as webapp
+
---
+++ constants/images.js
@@ -0,0 +1,17 @@
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.png";
+import cards from "../assets/images/cards.png";
+import path from "../assets/images/path.png";
+import logo from "../assets/images/logo.png";
+import logoSmall from "../assets/images/logo-small.png";
+import empty from "../assets/images/empty.png";
+import image3 from "../assets/images/image3.png";
+import image2 from "../assets/images/image2.png";
+import image1 from "../assets/images/image1.png";
+import image4 from "../assets/images/image4.png";
+import step1 from "../assets/images/step1.png";
+import step2 from "../assets/images/step2.png";
+import step3 from "../assets/images/step3.png";
+
+
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty,image3 ,image2,image1,image4, step1, step2, step3};
